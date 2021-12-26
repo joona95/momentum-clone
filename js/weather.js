1ded1f685e88edb1d9bc6340c0dab70a50249f38
joona95 <joona95@naver.com>
2021-12-27 00:00:13 +0900

    Add html, js, css for weather display

---
+++ js/weather.js
@@ -0,0 +1,34 @@
+import { API_KEY } from "./const.js";
+
+let weatherIcons = null;
+fetch("../icons.json").then(res => {
+    return res.json();
+}).then(data => {weatherIcons = data;});
+console.log(weatherIcons);
+
+function onGeoSuccess(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr` // units를 통해서 화씨를 섭씨로 변경
+    fetch(url).then(response => response.json()).then(data => {
+        const weatherContent = document.querySelector("#weather");
+        const i = document.createElement("i");
+        const icon = 'wi-owm-'+data.weather[0].id;
+        i.classList.add("wi");
+        i.classList.add(icon);
+        weatherContent.appendChild(i);
+        const temperature = document.createElement("span");
+        temperature.innerText = ` ${data.main.temp}℃`;
+        temperature.style.fontSize="20px";
+        weatherContent.appendChild(temperature);
+        const info = document.createElement("p");
+        info.innerText = `${data.weather[0].main} / ${data.name}`;        
+        weatherContent.appendChild(info);
+    }); 
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
